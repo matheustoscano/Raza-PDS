a89f21f24ef500d7a5d1728d192b5bef626f44af
Matheus <teustoscano@gmail.com>
2015-06-04 10:50:07 -0300

    Fixing a bug on menu.rb

--- Classes/menu.rb
+++ Classes/menu.rb
@@ -1,20 +1,26 @@
 puts "Raza -  O jogo de Azar!"
 
-puts "1 - Login"
-puts "2 - Sign in"
-puts "3 - Sair"
+	puts "1 - Login"
+	puts "2 - Sign in"
+	puts "3 - Sair"
 
-puts "Escolha uma das opções: "
-user_menu_choice = gets.to_i
+	puts "Escolha uma das opções: "
+	user_menu_choice = gets.to_i
 
-if user_menu_choice == 1
-	puts "Teu nome"
-	puts "Tua senha"
-end
-if user_menu_choice == 2
-	puts "Escolher username:"
-	puts "Escolher uma senha:"
-end
-if user_menu_choice == 3
-	puts "Encerrando..."
-end
+	if user_menu_choice == 1
+		puts "Teu nome"
+		puts "Tua senha"
+	end
+
+	if user_menu_choice == 2
+		puts "Escolher username:"
+		puts "Escolher uma senha:"
+	end
+
+	if user_menu_choice == 3
+		puts "Encerrando..."
+	end
+	
+	unless (1..3).include?(user_menu_choice)
+			puts "Por favor digite um número válido!"
+	end
