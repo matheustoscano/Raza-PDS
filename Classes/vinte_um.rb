b9f50163f78d58c9836ac5ceb137264487674fec
Matheus <teustoscano@gmail.com>
2015-06-07 13:21:17 -0300

    Creating a if statement

--- Classes/vinte_um.rb
+++ Classes/vinte_um.rb
@@ -4,6 +4,7 @@ class Vinte_Um
 
 dealer_hand = nil
 player_hand = nil
+user_card_choice = ""
 
 puts "Embaralhando!\n"
 puts "Entregando as cartas!\n\n"
@@ -12,11 +13,17 @@ player_hand = rand(1..10)
 puts "Sua mão: #{player_hand}"
 
 puts "Deseja receber mais uma carta?(s - n)"
-user_card_choice = gets.to_s
+user_card_choice = STDIN.getc
 
-if user_card_choice = "n"
+if user_card_choice == "n"
 	player_hand += rand(1..10)	
 	puts "Sua mão final: #{player_hand}"
+end
+if user_card_choice == "s"
+	player_hand += rand(1..10)
+	puts "Tua mão parcial: #{player_hand}"
+	puts "Deseja receber mais uma carta?(s - n)"
+	
 end
 
 end
